061083f8a95a8446d165d57dc6889f6f36c2f68c
caoyufei <atcaoyufei@gmail.com>
2020-10-17 13:42:41 +0800

    'glados'

--- clients/glados.py
+++ clients/glados.py
@@ -12,7 +12,8 @@ class Glados(BaseClient):
 
     def run(self, **kwargs):
         try:
-            cookie = base64.b64decode(kwargs.get('cookie')).decode('utf-8')
+            # cookie = base64.b64decode(kwargs.get('cookie')).decode('utf-8')
+            cookie = 'koa:sess=eyJ1c2VySWQiOjQyMzU2LCJfZXhwaXJlIjoxNjI3MzY2NDUzMzU3LCJfbWF4QWdlIjoyNTkyMDAwMDAwMH0=; koa:sess.sig=1cAg4N7AurgOGQ6DObSAi2qmwJ4'
             self.logger.info(self.checkin(cookie))
         except Exception as e:
             print(e)
@@ -20,7 +21,7 @@ class Glados(BaseClient):
     def checkin(self, cookie):
         url = "https://glados.rocks/api/user/checkin"
         referer = 'https://glados.rocks/console/checkin'
-        result = requests.post(url, headers={'cookie': cookie, 'referer': referer}).json()
+        result = requests.post(url, {'token': 'glados_network'}, headers={'cookie': cookie, 'referer': referer}).json()
         if result['code'] != 0:
             self.send_tg(f'GlaDos cookie has expired\n{result}')
         return result['message']
