8106b25b0737a64f499f58355c1beac3c0e10c49
caoyufei <atcaoyufei@gmail.com>
2020-12-30 09:12:01 +0800

    'appletuan'

--- clients/appletuan.py
+++ clients/appletuan.py
@@ -13,27 +13,31 @@ class AppleTuan(BaseClient):
 
     def run(self, **kwargs):
         try:
-            cookie = '_qddaz=QD.qwvb5b.ae718f.kfxdha0c; pgv_pvi=9167585280; tencentSig=1203038208; _session_id=tj6spRfBtZp7Mmb%2BJGuIttMcTUKnfzHJFaXRJsLaWqI0B8G7dy5DiMQkFBz6nPp%2BKZZiwe9VjdYx05dcKS4NSTNMRhdgTXR0Th79LVdRsgNyzRuqKhces3kNJMgFvzerf5f0DlOvpTZ%2FYQ%2Feo9SEq4YKl6moGYZTiHBSyintS7LEk7t7JEOyVeaTIraNJQHJleVJJBK%2B15RtNfNiujjZZexZGd83mmDucKyviYWsnUAS25G2Mv3ARcCBKnYqc%2Bsj%2BznP6nH3iTVs5Xq4xrH0at7HAbMH3aCgfzAL0w4BDNmLK44WUNjZ46DJQaEgey7STk0nGvRHJp2cqMUbqleTIRhTcWXYZX%2Fz1IuI4xRTAY%2Bewqo7xPPJx5ewLm9jxkK8idok0uoi5FL36Y6RjZp2VvSfiGO6TKTOw3J59Q09PgKF3GolHd57DLkrBKaC--5%2BmHNRAkerp0xZSj--MiNceJRc0di9ovrSdMT2NA%3D%3D'
+            cookie = '_qddaz=QD.qwvb5b.ae718f.kfxdha0c; pgv_pvi=9167585280; tencentSig=1203038208; _session_id=4rVjFLRnK9ZnWfqxVTxhoNSEDxFXNPanATmEuHe18Jcx2%2FhUEuzdrrXgSOCSOdguPsN87CCSgtVGGkC3GsGn56XEAFv8eWBnUG9Rk7WO03RLi56B3vPZuZQ0qQ3nT9IVfAUHeKiEC28BuQqLu1njrT%2BaveX4mudPiaBauiRJwsUfNWTjnPysBCuwAUTucszJpVjI2VSJGyE30%2FBejTgJMPlJHNwtUaP1eMJhwkLNVCute%2FEqm%2Fs5ExMCdcgBcVAq9byYTzB3bXWB14n4qVrskg1HFbFtXdpCVAtth%2BFOiqmXyQ1fJiJCyDHGR6E1OS07f9001j3tZgJaCShVLt8DXyDBCgwGTzm3oyfac1WMXYJOnT4td%2BPgnzDUWsXACVSuF0bM7de42XYEgYtW3BPchDafFORCRi23mx7PSxhCUpmf--lNfKIQQ9dnpiQmQU--Yd%2BL8JfrdffsEHq4V1LYdg%3D%3D'
             self.headers['Cookie'] = cookie
-            self.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
+            self.headers[
+                'User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36'
             html = self.fetch(f'{self.base_url}/checkins').text
             if html.find('users/sign_out') == -1:
                 self.logger.warning('not login.')
                 return
 
-            # doc = pyquery.PyQuery(html)
-            # credit = doc('.w-3\/12').eq(1).text().replace('0.00', '').strip()
-            # self.logger.info(f'credit -> {credit}')
+            doc = pyquery.PyQuery(html)
 
-            self.headers['Referer'] = f'{self.base_url}/checkins'
-            self.headers['Origin'] = self.base_url
-            self.headers['Host'] = 'www.appletuan.com'
-            token = re.search(r'<meta name="csrf-token" content="([^\"]+)" />', html).group(1)
-            params = {'_method': 'post', 'authenticity_token': token}
-
-            response = self.fetch(f'https://www.appletuan.com/checkins/start', params)
-            doc = pyquery.PyQuery(response.text)
             credit = doc('.w-3\/12').eq(1).text().replace('0.00', '').strip()
+            self.logger.info(f'credit -> {credit}')
+
+            token_name = doc('meta[name=csrf-param]').attr('content')
+            token = doc('meta[name=csrf-token]').attr('content')
+
+            self.headers['Referer'] = 'https://www.appletuan.com/checkins'
+            self.headers[
+                'Accept'] = 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+            params = {'_method': 'post', token_name: token}
+
+            response = self.fetch(f'https://www.appletuan.com/checkins/start', data=params)
+            doc = pyquery.PyQuery(response.text)
+            credit = doc('.w-3\/12').eq(1).text().replace('0.00', '').strip().split('\n')[0]
             self.logger.info(f'credit -> {credit}')
         except Exception as e:
             self.logger.error(e)
